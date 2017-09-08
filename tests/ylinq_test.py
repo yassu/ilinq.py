694b55514ee44f256cfe414831211f902ff16156
yassu <yassu0320.dev@gmail.com>
2017-09-08 22:44:30 +0900

    test_select

--- tests/ylinq_test.py
+++ tests/ylinq_test.py
@@ -19,8 +19,17 @@ class TestLinq(unittest.TestCase):
         assert(next(linq1) == 1)
 
     def test_where(self):
-        self.assertEqual(self.linq1.where(lambda x: x % 2 == 1).to_list(), [1])
-        self.assertEqual(self.linq2.where(lambda x: x % 2 == 0).to_list(), [2])
+        linq1 = Linq([1])
+        linq2 = Linq([1, 1, 2, 3, 5])
+        self.assertEqual(linq1.where(lambda x: x % 2 == 1).to_list(), [1])
+        self.assertEqual(linq2.where(lambda x: x % 2 == 0).to_list(), [2])
+
+    def test_select(self):
+        i = iter(range(5))
+        linq = Linq(i)
+        self.assertEqual(
+            linq.select(lambda x: x % 2 == 0).to_list(),
+            [True, False, True, False, True])
 
     def test_to_list(self):
         self.assertEqual(self.linq1.to_list(), [1])
