a6b79c40e5fede148a2aa62a5572009949a8ed97
yassu <yassu0320.dev@gmail.com>
2017-09-10 12:24:05 +0900

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(name='ilinq',
+      version='0.0.1',
+      description='linq library',
+      author='yassu',
+      author_email='yasu0320.dev@gmail.com',
+      url='https://github.com/yassu/Ilinq.py',
+      )
