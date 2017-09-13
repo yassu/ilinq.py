a1ef09cc4da6e7c10a06ca8f2466dff556ff101d
yassu <yassu0320.dev@gmail.com>
2017-09-14 02:40:10 +0900

    sample for tasks.py

--- tasks.py
+++ tasks.py
@@ -4,6 +4,12 @@
 from invoke import task
 
 
+def run_commands(ctx, commands):
+    for command in commands:
+        print(command)
+        ctx.run(command)
+
+
 @task
 def test(ctx):
     """ run flake8 and tests """
@@ -26,3 +32,22 @@ def clean_doc(ctx):
 def view_doc(ctx):
     """ open readme of sphinx docs"""
     ctx.run("open -a Google\ Chrome docs/_build/html/index.html")
+
+
+@task
+def deploy_doc(ctx):
+    build_doc(ctx)
+    run_commands(
+        ctx,
+        [
+            'cp -r docs/_build/html/ /tmp/.build',
+            'git checkout gh-pages',
+            'git clean -fdx',
+            'git rm -rf *',
+            'mv /tmp/.build/* .',
+            'touch .nojekyll',
+            'git add .',
+            'git commit -m "update docs"',
+            'git push origin gh-pages',
+            'git checkout master'
+        ])
