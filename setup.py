d9fc5cea4ce9b016144bda3c5019d7bdbf3105b7
yassu <yassu0320.dev@gmail.com>
2017-09-26 01:33:38 +0900

    add classifiers

--- setup.py
+++ setup.py
@@ -4,10 +4,24 @@
 from setuptools import setup
 from ilinq import __VERSION__
 
-setup(name='ilinq',
-      version=__VERSION__,
-      description='linq library',
-      author='yassu',
-      author_email='yasu0320.dev@gmail.com',
-      url='https://github.com/yassu/ilinq.py',
-      )
+classifiers = [
+    "Development Status :: 3 - Alpha",
+    "License :: OSI Approved :: Apache Software License",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3 :: Only",
+    "Intended Audience :: Developers",
+    "Operating System :: OS Independent",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+    "Topic :: Utilities",
+]
+
+setup(
+    name='ilinq',
+    version=__VERSION__,
+    description='linq library',
+    author='yassu',
+    author_email='yasu0320.dev@gmail.com',
+    classifiers=classifiers,
+    url='https://github.com/yassu/ilinq.py',
+  )
