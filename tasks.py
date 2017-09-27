13c3e3d62e75bcc42c4916b3949abb41510b5ab8
yassu <yassu0320.dev@gmail.com>
2017-09-28 01:57:54 +0900

    test with coverage-html

--- tasks.py
+++ tasks.py
@@ -14,7 +14,7 @@ def run_commands(ctx, commands):
 def test(ctx):
     """ run flake8 and tests """
     ctx.run('flake8')
-    ctx.run('nosetests --with-coverage')
+    ctx.run('nosetests --with-coverage --cover-branches --cover-html')
 
 
 @task
