70ad94ad564f8621af25b294049e1ebea0fe5b5d
yassu <yassu0320.dev@gmail.com>
2019-04-04 01:27:28 +0900

    bump ver

--- ilinq/__init__.py
+++ ilinq/__init__.py
@@ -6,5 +6,5 @@ def _get_short_version(full_version):
     return '.'.join(full_version.split('.')[:2])
 
 
-__VERSION__ = '0.3.0'
+__VERSION__ = '0.3.1'
 __SHORT_VERSION__ = _get_short_version(__VERSION__)
